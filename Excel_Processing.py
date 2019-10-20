bb14b4462abff872636e7ead507b5fa58492b60a
Chris B <bushmecj@gmail.com>
2019-10-20 09:40:42 -0500

    Init commit

---
+++ Excel_Processing.py
@@ -0,0 +1,36 @@
+'''
+Description: This program will accept an excel file and automatically process it.
+Processing will include aggregating floats by name.
+
+Author: Chris Bushmeyer
+
+Created: 10/20/19
+
+Last Modified: 10/20/19
+'''
+
+import pandas as pd
+import getopt
+import xlsxwriter
+import os
+
+
+
+def main():
+    pass
+    '''
+    Steps
+    1. Add opt step for CLI processing
+    2. Check file is an xlsx file
+    3. Read into pandas
+    4. Process
+    5. Write out using xlslx writer
+
+    Final:
+    Create .exe of python program
+
+    Future Changes
+    Add Flask? Module for GUI interface replacement
+    '''
+if __name__ == "__main__":
+    main()
